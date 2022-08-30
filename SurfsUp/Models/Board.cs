9a7236644e4ca4d9e3eef5537a78caea1dadb9f5
Marcus <mbje38194@edu.ucl.dk>
2022-08-30 09:34:04 +0200

    Validation logic added

--- SurfsUp/Models/Board.cs
+++ SurfsUp/Models/Board.cs
@@ -22,17 +22,40 @@ namespace SurfsUp.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
         //public string Description { get; set; }
+
+        [Range(1, 100)]
+        [Required]
         public float Length { get; set; }
+
+        [Range(1, 100)]
+        [Required]
         public float Width { get; set; }
+
+        [Range(1, 100)]
+        [Required]
         public float Thickness { get; set; }
+
+        [Range(1, 1000)]
+        [Required]
         public float Volume { get; set; }
+
+        [Required]
         public BoardType Type { get; set; }
 
+        [Range(1, 10000)]
         [Column(TypeName = "decimal(18, 2)")]
+        [Required]
         public decimal Price { get; set; }
+
+
         public string Equipments { get; set; }
+
+        [Required]
         public string ImageURL { get; set; }
 
         //public ICollection<Equipment> Equipments { get; set; }
