bd59cfbaaa3661c9ffdcde91434e2e3373268c64
Kai Bergmann <90706342+KaiBerg@users.noreply.github.com>
2022-11-04 11:02:03 +0100

    added weather component

---
+++ Blazor/Program.cs
@@ -0,0 +1,11 @@
+using Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/") });
+
+await builder.Build().RunAsync();
